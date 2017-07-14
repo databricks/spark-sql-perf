289fc1bb9d32846e5e6423925b5ceea5d3f25e39
Bogdan Raducanu <bogdan@databricks.com>
2017-07-14 14:21:21 +0200

    removed skew queries from TPCH

--- src/main/scala/com/databricks/spark/sql/perf/tpch/TPCH.scala
+++ src/main/scala/com/databricks/spark/sql/perf/tpch/TPCH.scala
@@ -179,11 +179,4 @@ class TPCH(@transient sqlContext: SQLContext)
   }
   val queriesMap = queries.map(q => q.name.split("-").get(0) -> q).toMap
 
-  val queriesSkew = (1 to 22).map { q =>
-    val queryContent: String = IOUtils.toString(
-      getClass().getClassLoader().getResourceAsStream(s"tpch/queries.skew/$q.sql"))
-    Query(s"Q$q.skew", queryContent, description = "TPCH Query with Skew",
-      executionMode = CollectResults)
-  }
-  val queriesSkewMap = queriesSkew.map(q => q.name.split("-").get(0) -> q).toMap
 }
