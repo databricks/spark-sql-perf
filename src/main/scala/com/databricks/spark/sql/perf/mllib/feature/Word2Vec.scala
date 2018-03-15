5af9f6dfc20ba175c588b401b25de107b5c6bbbf
WeichenXu <weichen.xu@databricks.com>
2018-03-16 04:10:04 +0800

    Word2Vec benchmark (#127)
    * init pr
    
    * update
    
    * use builtin split fun

---
+++ src/main/scala/com/databricks/spark/sql/perf/mllib/feature/Word2Vec.scala
@@ -0,0 +1,34 @@
+package com.databricks.spark.sql.perf.mllib.feature
+
+import org.apache.spark.ml
+import org.apache.spark.ml.PipelineStage
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.{col, split}
+
+import com.databricks.spark.sql.perf.mllib.{BenchmarkAlgorithm, MLBenchContext, TestFromTraining}
+import com.databricks.spark.sql.perf.mllib.OptionImplicits._
+import com.databricks.spark.sql.perf.mllib.data.DataGenerator
+
+/** Object for testing Word2Vec performance */
+object Word2Vec extends BenchmarkAlgorithm with TestFromTraining {
+
+  override def trainingDataSet(ctx: MLBenchContext): DataFrame = {
+    import ctx.params._
+
+    val df = DataGenerator.generateDoc(
+      ctx.sqlContext,
+      numExamples,
+      ctx.seed(),
+      numPartitions,
+      vocabSize,
+      docLength,
+      "text"
+    )
+    df.select(split(col("text"), " ").as("text"))
+  }
+
+  override def getPipelineStage(ctx: MLBenchContext): PipelineStage = {
+    new ml.feature.Word2Vec().setInputCol("text")
+  }
+
+}
