b8aeda96a07b4d4021945c2744b7df4034b32376
Bago Amirbekian <bago@databricks.com>
2018-05-08 01:16:40 -0700

    Check results in unit tests and fail tests if failures are present.

--- src/test/scala/com/databricks/spark/sql/perf/mllib/MLLibTest.scala
+++ src/test/scala/com/databricks/spark/sql/perf/mllib/MLLibTest.scala
@@ -5,7 +5,12 @@ import org.scalatest.FunSuite
 class MLLibTest extends FunSuite {
 
   test("test MlLib benchmarks with mllib-small.yaml.") {
-    MLLib.run(MLLib.smallConfig)
+    val results = MLLib.run(yamlConfig = MLLib.smallConfig)
+    val failures = results.na.drop(Seq("failure"))
+    if (failures.count > 0) {
+      val failed = failures.select("name").collect.map(_.getString(0)).mkString(", ")
+      fail(s"There were failures in the following benchmarks: $failed.")
+    }
   }
 
 }
