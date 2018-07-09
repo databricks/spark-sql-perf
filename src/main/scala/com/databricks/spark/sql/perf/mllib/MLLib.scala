320fb4dd738f06f9f0bffb505f4e4aa71e3d25a7
Bago Amirbekian <bago@databricks.com>
2018-07-09 16:20:20 -0700

    Added test for before & after methods of mllib pipeline-stage benchmarks.

--- src/main/scala/com/databricks/spark/sql/perf/mllib/MLLib.scala
+++ src/main/scala/com/databricks/spark/sql/perf/mllib/MLLib.scala
@@ -54,6 +54,21 @@ object MLLib extends Logging {
     run(yamlFile = configFile)
   }
 
+  private[mllib] def getConf(yamlFile: String = null, yamlConfig: String = null): YamlConfig = {
+    Option(yamlFile).map(YamlConfig.readFile).getOrElse {
+      require(yamlConfig != null)
+      YamlConfig.readString(yamlConfig)
+    }
+  }
+
+  private[mllib] def getBenchmarks(conf: YamlConfig): Seq[MLPipelineStageBenchmarkable] = {
+    val sqlContext = com.databricks.spark.sql.perf.mllib.MLBenchmarks.sqlContext
+    val benchmarksDescriptions = conf.runnableBenchmarks
+    benchmarksDescriptions.map { mlb =>
+      new MLPipelineStageBenchmarkable(mlb.params, mlb.benchmark, sqlContext)
+    }
+  }
+
   /**
    * Runs all the experiments and blocks on completion
    *
@@ -62,20 +77,12 @@ object MLLib extends Logging {
    */
   def run(yamlFile: String = null, yamlConfig: String = null): DataFrame = {
     logger.info("Starting run")
-    val conf: YamlConfig = Option(yamlFile).map(YamlConfig.readFile).getOrElse {
-      require(yamlConfig != null)
-      YamlConfig.readString(yamlConfig)
-    }
-
+    val conf = getConf(yamlFile, yamlConfig)
     val sparkConf = new SparkConf().setAppName("MLlib QA").setMaster("local[2]")
     val sc = SparkContext.getOrCreate(sparkConf)
     sc.setLogLevel("INFO")
     val b = new com.databricks.spark.sql.perf.mllib.MLLib()
-    val sqlContext = com.databricks.spark.sql.perf.mllib.MLBenchmarks.sqlContext
-    val benchmarksDescriptions = conf.runnableBenchmarks
-    val benchmarks = benchmarksDescriptions.map { mlb =>
-      new MLPipelineStageBenchmarkable(mlb.params, mlb.benchmark, sqlContext)
-    }
+    val benchmarks = getBenchmarks(conf)
     println(s"${benchmarks.size} benchmarks identified:")
     val str = benchmarks.map(_.prettyPrint).mkString("\n")
     println(str)
--- src/test/scala/com/databricks/spark/sql/perf/mllib/MLLibSuite.scala
+++ src/test/scala/com/databricks/spark/sql/perf/mllib/MLLibSuite.scala
@@ -1,10 +1,29 @@
 package com.databricks.spark.sql.perf.mllib
 
-import org.scalatest.FunSuite
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
 
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.Row
 
-class MLLibTest extends FunSuite {
+class MLLibSuite extends FunSuite with BeforeAndAfterAll {
+
+  private var sc: SparkContext = _
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    val sparkConf = new SparkConf().setAppName("MLlib QA").setMaster("local[2]")
+    sc = SparkContext.getOrCreate(sparkConf)
+  }
+
+  override def afterAll(): Unit = {
+    super.afterAll()
+    if (sc != null) {
+      sc.stop()
+    }
+    // To avoid RPC rebinding to the same port, since it doesn't unbind immediately on shutdown
+    System.clearProperty("spark.driver.port")
+    sc = null
+  }
 
   test("test MlLib benchmarks with mllib-small.yaml.") {
     val results = MLLib.run(yamlConfig = MLLib.smallConfig)
@@ -18,6 +37,14 @@ class MLLibTest extends FunSuite {
              """.stripMargin)
       }
       fail("Unable to run all benchmarks successfully, see console output for more info.")
+    }
+  }
+
+  test("test before & after benchmark methods for pipeline benchmarks.") {
+    val benchmarks = MLLib.getBenchmarks(MLLib.getConf(yamlConfig = MLLib.smallConfig))
+    benchmarks.foreach { b =>
+      b.beforeBenchmark()
+      b.afterBenchmark(sc)
     }
   }
 }
