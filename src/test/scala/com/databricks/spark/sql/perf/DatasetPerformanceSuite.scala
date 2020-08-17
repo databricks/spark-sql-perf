39fbb0e9bd80e4fcbbc580a04b6f0197393cc796
npoggi <npmnpm@gmail.com>
2020-08-17 22:42:20 +0200

    Remove hive.test dependency (failing in spark 3.0.0)

--- src/test/scala/com/databricks/spark/sql/perf/DatasetPerformanceSuite.scala
+++ src/test/scala/com/databricks/spark/sql/perf/DatasetPerformanceSuite.scala
@@ -1,11 +1,9 @@
 package com.databricks.spark.sql.perf
 
-import org.apache.spark.sql.hive.test.TestHive
 import org.scalatest.FunSuite
 
 class DatasetPerformanceSuite extends FunSuite {
   ignore("run benchmark") {
-    TestHive // Init HiveContext
     val benchmark = new DatasetPerformance() {
       override val numLongs = 100
     }
